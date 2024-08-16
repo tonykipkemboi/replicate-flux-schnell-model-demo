13140a324f2be712af2350a78488d629de7d0d1e
Tony Kipkemboi <iamtonykipkemboi@gmail.com>
2024-08-16 17:21:55 -0400

    UI improvements

--- demo.py
+++ demo.py
@@ -1,68 +1,81 @@
-import replicate
 import streamlit as st
 from replicate.client import Client
 from io import BytesIO
 from PIL import Image
 import requests
 
-st.set_page_config(page_title="Flux-Schnell Model Demo",
-                   page_icon="🎨", layout="wide")
 
-st.title("Dream Canvas 🎨")
+def main():
+    st.set_page_config(
+        page_title="Flux-Schnell Model Demo",
+        page_icon="🎨",
+        layout="wide"
+    )
 
-REPLICATE_API_TOKEN = st.secrets["REPLICATE_API_TOKEN"]
-MODEL = "black-forest-labs/flux-schnell"
-client = Client(api_token=REPLICATE_API_TOKEN)
+    st.title("Dream Canvas 🎨")
 
-prompt = st.text_area(
-    "Type your prompt...",
-    value='black forest gateau cake spelling out the words "SETHYBOO", tasty, food photography, dynamic shot',
-)
+    replicate_api_token = st.secrets["REPLICATE_API_TOKEN"]
+    model = "black-forest-labs/flux-schnell"
+    client = Client(api_token=replicate_api_token)
 
-aspect_ratio = st.sidebar.selectbox(
-    "Aspect Ratio",
-    options=["1:1", "16:9", "21:9", "2:3",
-             "3:2", "4:5", "5:4", "9:16", "9:21"],
-)
+    prompt = st.text_area(
+        "Type your prompt...",
+        value='black forest gateau cake spelling out the words "SETHYBOO", tasty, food photography, dynamic shot',
+    )
 
-output_format = st.sidebar.selectbox(
-    "Select Output Format", options=["webp", "png", "jpg"]
-)
+    aspect_ratio = st.sidebar.selectbox(
+        "Aspect Ratio",
+        options=["1:1", "16:9", "21:9", "2:3",
+                 "3:2", "4:5", "5:4", "9:16", "9:21"],
+    )
 
-output_quality = st.sidebar.slider(
-    "Output Quality", min_value=0, max_value=100, value=90
-)
+    output_format = st.sidebar.selectbox(
+        "Select Output Format", options=["png", "jpg", "webp"]
+    )
 
-_, col2, _ = st.columns(3)
+    output_quality = st.sidebar.slider(
+        "Output Quality", min_value=0, max_value=100, value=90
+    )
 
-with col2:
-    if st.button("📸 Generate", type="primary"):
-        output = client.run(
-            MODEL,
-            input={
-                "prompt": prompt,
-                "num_outputs": 1,
-                "aspect_ratio": aspect_ratio,
-                "output_format": output_format,
-                "output_quality": output_quality,
-            },
-        )
+    _, col2, _ = st.columns(3)
 
-        image_url = output[0]
-        image = Image.open(BytesIO(requests.get(image_url).content))
+    with col2:
+        if st.button("📸 Generate Image", type="primary", use_container_width=True):
+            generate_and_display_image(
+                client, model, prompt, aspect_ratio, output_format, output_quality)
 
-        st.image(image, caption=prompt)
 
-        # Prepare the image for download
-        img_buffer = BytesIO()
-        image.save(img_buffer, format=output_format.upper())
-        img_buffer.seek(0)
+def generate_and_display_image(client, model, prompt, aspect_ratio, output_format, output_quality):
+    output = client.run(
+        model,
+        input={
+            "prompt": prompt,
+            "num_outputs": 1,
+            "aspect_ratio": aspect_ratio,
+            "output_format": output_format,
+            "output_quality": output_quality,
+        },
+    )
 
-        # Add download button
-        st.download_button(
-            label="Download Image",
-            data=img_buffer,
-            file_name=f"generated_image.{output_format}",
-            mime=f"image/{output_format}",
-            use_container_width=True,
-        )
+    image_url = output[0]
+    image = Image.open(BytesIO(requests.get(image_url).content))
+
+    st.image(image, caption=prompt)
+
+    # Prepare the image for download
+    img_buffer = BytesIO()
+    image.save(img_buffer, format=output_format.upper())
+    img_buffer.seek(0)
+
+    # Add download button
+    st.download_button(
+        label="Download Image",
+        data=img_buffer,
+        file_name=f"generated_image.{output_format}",
+        mime=f"image/{output_format}",
+        use_container_width=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
